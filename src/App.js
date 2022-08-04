4c6ae0c6decf4fc3ef183d34261c2950f5bb14f4
shaqdeff <shaquillendunda@gmail.com>
2022-08-04 14:29:49 +0300

    Finish header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import Header from './components/header/Header';
 import Home from './pages/Home';
 import Details from './pages/Details';
