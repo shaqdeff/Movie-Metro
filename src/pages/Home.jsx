f211279e78dc8ef7d773f24747100272d11abb3b
shaqdeff <shaquillendunda@gmail.com>
2022-08-05 07:42:28 +0300

    Dispactch addMovies action to store

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import MovieList from '../components/movieList/MovieList';
 import movieApi from '../api/movieApi';
 import APIKey from '../api/movieApiKey';
+import { addMovies } from '../redux/moviesSlice/moviesSlice';
 
 /* eslint-disable arrow-body-style */
 const Home = () => {
   const movieText = 'Harry';
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
         .catch((error) => console.log('Error: ', error));
-      console.log('Response: ', response);
+      dispatch(addMovies(response.data));
     };
+
     fetchMovies();
   }, []);
 
--- src/redux/moviesSlice/moviesSlice.js
+++ src/redux/moviesSlice/moviesSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  movies: [],
+  movies: {},
 };
 
 /* eslint-disable no-param-reassign */
