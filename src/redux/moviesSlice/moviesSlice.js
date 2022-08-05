83755127639c996843c8f404725ae7bff94bb2f5
shaqdeff <shaquillendunda@gmail.com>
2022-08-05 07:30:34 +0300

    Create movies slice in redux

---
+++ src/redux/moviesSlice/moviesSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  movies: [],
+};
+
+/* eslint-disable no-param-reassign */
+const moviesSlice = createSlice({
+  name: 'movies',
+  initialState,
+  reducers: {
+    addMovies: (state, { payload }) => {
+      state.movies = payload;
+    },
+  },
+});
+
+export const { addMovies } = moviesSlice.actions;
+export const getAllMovies = (state) => state.movies.movies;
+export default moviesSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import moviesReducer from './moviesSlice/moviesSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    moviesReducer,
+  },
 });
 
 export default store;
